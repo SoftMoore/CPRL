916cf6993387587210a9ae89467581edbd42b06a
SoftMoore <jmoore@softmoore.com>
2020-06-22 17:48:44 -0400

    Simplified Type.hashCode()

--- Student/src-ParserV3/src/edu/citadel/cprl/Type.java
+++ Student/src-ParserV3/src/edu/citadel/cprl/Type.java
@@ -113,10 +113,7 @@ public class Type
     @Override
     public int hashCode()
       {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+        return name == null ? 0 : name.hashCode();
       }
 
 
